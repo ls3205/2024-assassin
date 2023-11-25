4dd951a6d06f5cf4401e0cf3274566a283ca457a
ls3205 <ls3205@trevor.org>
2023-11-24 19:27:19 -0500

    set up initial auth overhead

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import type { Session, User } from "next-auth"
+import type { JWT } from "next-auth/jwt"
+
+type UserId = string
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id: UserId
+        role: "ADMIN" | "PLAYER"
+        status: "ALIVE" | "DEAD"
+    }
+}
+
+declare module "next-auth" {
+    interface Session {
+        user: User & {
+            id: UserId
+            role: "ADMIN" | "PLAYER"
+            status: "ALIVE" | "DEAD"
+            picture?: string | null
+        }
+    }
+}
