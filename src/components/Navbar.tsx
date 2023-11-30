6bacbd7e5881aea65a3eb4036f8bf751609f09f0
ls3205 <ls3205@trevor.org>
2023-11-30 08:36:25 -0500

    started navbar reactive styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import ThemeDropdown from "./ThemeDropdown";
 import Link from "next/link";
 import { Button } from "./ui/Button";
 import MobileNavbar from "./MobileNavbar";
+import NavbarLinks from "./NavbarLinks";
 
 interface NavbarProps {}
 
@@ -16,21 +17,7 @@ const Navbar: React.FC<NavbarProps> = async ({}) => {
             <div className="relative hidden w-full flex-row items-center justify-center md:flex">
                 <div className="relative flex w-[70%] flex-row justify-center border-b-[1px] border-primary p-5">
                     <div className="relative mr-auto flex w-1/2 flex-row items-center justify-start gap-5">
-                        <Button variant={"ghost"}>
-                            <Link href={"/"}>Home</Link>
-                        </Button>
-                        {session?.user &&
-                            (session.user.role === "ADMIN" ? (
-                                <Button variant={"ghost"}>
-                                    <Link href={"/admin"}>Admin Dashboard</Link>
-                                </Button>
-                            ) : (
-                                <Button variant={"ghost"}>
-                                    <Link href={"/player"}>
-                                        Player Dashboard
-                                    </Link>
-                                </Button>
-                            ))}
+                        <NavbarLinks session={session} />
                     </div>
                     <div className="relative ml-auto flex w-1/2 flex-row items-center justify-end gap-5">
                         {session?.user ? (
---
+++ src/components/NavbarLinks.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Session } from "next-auth";
+import React from "react";
+import { Button } from "./ui/Button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+interface NavbarLinksProps {
+    session: Session | null;
+}
+
+const NavbarLinks: React.FC<NavbarLinksProps> = ({ session }) => {
+    const pathname = usePathname();
+
+    return (
+        <>
+            <Button variant={"ghost"}>
+                <Link
+                    className={cn(
+                        pathname === "/" ? "text-primary" : "text-foreground",
+                    )}
+                    href={"/"}
+                >
+                    Home
+                </Link>
+            </Button>
+            <Button variant={"ghost"}>
+                <Link className={cn(pathname === "/players" ? "text-primary" : "text-foreground")} href={"/players"}>
+                    Players
+                </Link>
+            </Button>
+            <Button variant={"ghost"}>
+                <Link className={cn(pathname === "/admin" ? "text-primary" : "text-foreground")} href={"/kills"}>
+                    Kills
+                </Link>
+            </Button>
+            {session?.user &&
+                (session.user.role === "ADMIN" ? (
+                    <Button variant={"ghost"}>
+                        <Link
+                            className={cn(
+                                pathname === "/admin"
+                                    ? "text-primary"
+                                    : "text-foreground",
+                            )}
+                            href={"/admin"}
+                        >
+                            Admin Dashboard
+                        </Link>
+                    </Button>
+                ) : (
+                    <Button variant={"ghost"}>
+                        <Link
+                            className={cn(
+                                pathname === "/player"
+                                    ? "text-primary"
+                                    : "text-foreground",
+                            )}
+                            href={"/player"}
+                        >
+                            Player Dashboard
+                        </Link>
+                    </Button>
+                ))}
+        </>
+    );
+};
+
+export default NavbarLinks;
