e474c41d8e8438cd26d8fecad35eb11f49698eef
ls3205 <ls3205@trevor.org>
2023-12-03 17:58:47 -0500

    target pairings page

---
+++ src/app/(dashboards)/admin/pairings/page.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/Navbar";
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import React from "react";
+
+interface pageProps {}
+
+const page: React.FC<pageProps> = async ({}) => {
+    const session = await getAuthSession();
+
+    return !session || session.user.role !== "ADMIN" ? (
+        redirect("/")
+    ) : (
+        <main className="flex min-h-screen flex-col items-center">
+            <Navbar />
+        </main>
+    );
+};
+
+export default page;
