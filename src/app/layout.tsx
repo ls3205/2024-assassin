026a39e202faf4062e411c0063bbba97dec24b4e
ls3205 <ls3205@trevor.org>
2023-12-12 09:32:09 -0500

    and now it just redirects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
         <html lang="en">
             <body className={`font-sans ${inter.variable}`}>
                 <Providers>
-                    {children}
                     {killConfirmModal}
+                    {children}
                 </Providers>
             </body>
         </html>
