1e58cbed609fa9b084219bf296898f7698e4f105
ls3205 <ls3205@trevor.org>
2024-01-09 13:21:46 -0500

    started working on player dashboard but will rework

--- src/app/(dashboards)/player/page.tsx
+++ src/app/(dashboards)/player/page.tsx
@@ -1,13 +1,21 @@
 import Navbar from "@/components/Navbar";
+import PlayerCard from "@/components/PlayerCard";
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 interface pageProps {}
 
-const page: React.FC<pageProps> = ({}) => {
-    return (
+const page: React.FC<pageProps> = async ({}) => {
+    const session = await getAuthSession();
+
+    return session && session.user && session.user.id ? (
         <main className="flex min-h-screen flex-col items-center">
-            {/* <Navbar /> */}
+            <Navbar />
+            <PlayerCard playerID={session.user.id} session={session} />
         </main>
+    ) : (
+        redirect("/")
     );
 };
 
