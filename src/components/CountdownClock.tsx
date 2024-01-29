280480fe478c4f49ae8ed6b0e45c1a6b2ce95f25
ls3205 <ls3205@trevor.org>
2024-01-28 23:14:58 -0500

    fixed countdown text styling

--- src/components/CountdownClock.tsx
+++ src/components/CountdownClock.tsx
@@ -36,10 +36,10 @@ const CountdownClock: React.FC<CountdownClockProps> = ({
         const totalSeconds = Math.floor(timeLeftms / 1000);
         const totalMinutes = Math.floor(totalSeconds / 60);
         const totalHours = Math.floor(totalMinutes / 60);
-        const totalDays = Math.floor(totalHours / 24);
-        const remSeconds = totalSeconds % 60;
-        const remMinutes = totalMinutes % 60;
-        const remHours = totalHours % 24;
+        const totalDays = Math.floor(totalHours / 24) < 10 ? `0${Math.floor(totalHours / 24)}` : Math.floor(totalHours / 24);
+        const remSeconds = totalSeconds % 60 < 10 ? `0${totalSeconds % 60}` : totalSeconds % 60;
+        const remMinutes = totalMinutes % 60 < 10 ? `0${totalMinutes % 60}` : totalMinutes % 60;
+        const remHours = totalHours % 24 < 10 ? `0${totalHours % 24}` : totalHours % 24;
 
         return `${totalDays}:${remHours}:${remMinutes}:${remSeconds}`;
     };
