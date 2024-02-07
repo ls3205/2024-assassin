7e69b4a12060f31bd83b54924707580b7b9cf6e2
ls3205 <ls3205@trevor.org>
2024-02-07 14:19:12 -0500

    finished bounty form

--- src/components/BountyForm.tsx
+++ src/components/BountyForm.tsx
@@ -5,19 +5,41 @@ import { useQuery } from "@tanstack/react-query";
 import { User } from "next-auth";
 import React from "react";
 import { z } from "zod";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/Form";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "./ui/Form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "./ui/Input";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "./ui/Select";
+import { Button } from "./ui/Button";
 
 interface BountyFormProps {
     user: Pick<User, "name" | "email" | "image"> | undefined;
 }
 
 const formSchema = z.object({
-    Target: z.string().min(25).max(25),
+    Target: z.string(),
     CreatorName: z.string(),
-    Amount: z.number().positive().max(100, "Bounty must be below $100."),
+    Amount: z.preprocess(
+        (x) => Number(x),
+        z
+            .number()
+            .positive("You cannot submit a negative bounty.")
+            .max(100, "Bounty must be below $100"),
+    ),
 });
 
 const BountyForm: React.FC<BountyFormProps> = ({ user }) => {
@@ -30,7 +52,9 @@ const BountyForm: React.FC<BountyFormProps> = ({ user }) => {
         },
     });
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {};
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
 
     const { isLoading, error, data } = useQuery({
         queryKey: ["BountyFormDataGet"],
@@ -49,7 +73,7 @@ const BountyForm: React.FC<BountyFormProps> = ({ user }) => {
     }
 
     return (
-        <>
+        <div className="m-4 rounded-lg bg-secondary/80 p-4">
             {data.length === 0 ? (
                 <div></div>
             ) : (
@@ -62,24 +86,31 @@ const BountyForm: React.FC<BountyFormProps> = ({ user }) => {
                                 <FormItem>
                                     <FormLabel>Target</FormLabel>
                                     <FormControl>
-                                        <select className="text-black">
-                                            <option value="" />
-                                            {data.map((user) => {
-                                                return (
-                                                    <option value={user.id}>
-                                                        {user.name}
-                                                    </option>
-                                                );
-                                            })}
-                                        </select>
+                                        <Select onValueChange={field.onChange}>
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Select a Player" />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                {data.map((user) => {
+                                                    return (
+                                                        <SelectItem
+                                                            value={user.id}
+                                                        >
+                                                            {user.name}
+                                                        </SelectItem>
+                                                    );
+                                                })}
+                                            </SelectContent>
+                                        </Select>
                                     </FormControl>
                                     <FormDescription>
-                                        The player you want to place a bounty on.
+                                        The player you want to place a bounty
+                                        on.
                                     </FormDescription>
                                     <FormMessage />
                                 </FormItem>
                             )}
-                        ></FormField>
+                        />
                         <FormField
                             control={form.control}
                             name="Amount"
@@ -87,19 +118,56 @@ const BountyForm: React.FC<BountyFormProps> = ({ user }) => {
                                 <FormItem>
                                     <FormLabel>Amount</FormLabel>
                                     <FormControl>
-                                        <Input placeholder="0.00" type="number" {...field}></Input>
+                                        <Input
+                                            placeholder="$0.00"
+                                            type="number"
+                                            {...field}
+                                        ></Input>
                                     </FormControl>
                                     <FormDescription>
-                                        The amount of money you want to place on the player.
+                                        The amount of money you want to place on
+                                        the player.
                                     </FormDescription>
                                     <FormMessage />
                                 </FormItem>
                             )}
-                        ></FormField>
+                        />
+                        <FormField
+                            control={form.control}
+                            name="CreatorName"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        {user?.name ? (
+                                            <Input
+                                                placeholder={
+                                                    form.getValues().CreatorName
+                                                }
+                                                disabled
+                                            />
+                                        ) : (
+                                            <Input
+                                                placeholder="Name"
+                                                disabled
+                                                {...field}
+                                            ></Input>
+                                        )}
+                                    </FormControl>
+                                    <FormDescription>
+                                        Your Name.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" variant={"default"}>
+                            Submit
+                        </Button>
                     </form>
                 </Form>
             )}
-        </>
+        </div>
     );
 };
 
