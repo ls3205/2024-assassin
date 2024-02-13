81ccd697b8d8d853c4e603f4f82e17d94a522ab4
ls3205 <ls3205@trevor.org>
2024-02-13 14:49:51 -0500

    bounty create warnign

--- src/app/(overview)/bounties/create/page.tsx
+++ src/app/(overview)/bounties/create/page.tsx
@@ -1,19 +1,30 @@
 import BountyForm from "@/components/BountyForm";
 import Navbar from "@/components/Navbar";
 import { getAuthSession } from "@/lib/auth";
+import { AlertCircleIcon } from "lucide-react";
 import React from "react";
 
 interface pageProps {}
 
 const page: React.FC<pageProps> = async ({}) => {
-    const session = await getAuthSession()
+    const session = await getAuthSession();
 
     return (
         <main className="flex min-h-screen flex-col items-center">
             <Navbar />
+            <div className="my-4 flex w-[95%] flex-col justify-center rounded-lg bg-yellow-300 p-4 align-middle text-black md:w-[60%]">
+                <h1 className="flex w-full flex-row justify-center text-center text-2xl font-bold">
+                    <AlertCircleIcon className="mr-4" />
+                    Notice
+                </h1>
+                <h3 className="text-center text-xl">
+                    Money must be given to Leon before bounty is marked as
+                    confirmed becomes active.
+                </h3>
+            </div>
             <BountyForm user={session?.user} />
         </main>
-    )
+    );
 };
 
 export default page;
