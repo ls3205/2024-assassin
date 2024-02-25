f4eaa9812ad44d6daff75e9ffdb3801459a8c61c
ls3205 <ls3205@trevor.org>
2024-02-25 17:49:43 -0500

    remaining player counter

--- src/app/(overview)/players/page.tsx
+++ src/app/(overview)/players/page.tsx
@@ -9,12 +9,7 @@ const page: React.FC<pageProps> = ({}) => {
         <main className="flex min-h-screen flex-col items-center">
             <Navbar />
             <div className="flex w-[70%] flex-col items-center justify-center lg:flex-row lg:items-start">
-                <div className="m-4 w-full rounded-lg bg-secondary">
-                    <h1 className="my-2 w-full text-center text-2xl font-semibold">
-                        Players
-                    </h1>
-                    <PlayerList className="lg:basis-1/4" type="LINK" />
-                </div>
+                <PlayerList className="lg:basis-1/4" type="LINK" />
             </div>
         </main>
     );
